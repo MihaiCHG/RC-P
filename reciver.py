e01286f98cf14d4c69887121a16db65dd59fd6cd
MihaiCHG <52129403+MihaiCHG@users.noreply.github.com>
2019-12-02 17:55:09 +0200

    V0.2
    (+) Pierderea voita a pachetelor
    (+) Codul a fost aranjat

--- reciver.py
+++ reciver.py
@@ -1,57 +1,86 @@
-import socket
-Nack = 1
-Ack = 2
-Data = 4
-INF = 8
 
-try:
+import socket
+from random import random
+
+
+class Reciver:
+    def __init__(self, ip, port):
+        self.UDP_IP = ip
+        self.UDP_PORT = port
+        self.Nack = 1
+        self.Ack = 2
+        self.Data =4
+        self.INF = 8
+
+    def decodeData(self, packet):
+        pID = packet[1:5]
+        len = packet[5:9]
+        data = packet[9:]
+        return pID, len, data
+
+    def decodeINF(self, packet):
+        packets = packet[1:5]
+        fname = packet[5:]
+        return packets, fname
+
+    def encodeAck(self, pID):
+        toSend = b''
+        toSend = toSend + self.Ack.to_bytes(1, "big") + pID
+        return toSend
+
+    def start(self):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.bind((UDP_IP, UDP_PORT))
+            print(UDP_IP)
+            numberOfPackets = 0
+            fileName = ""
+            packetID = 0
+            length = 0
+            packetExpected = 1
+            file = 0
+            while True:
+                data, addr = sock.recvfrom(1024)
+                packet=bytes(data)
+                print(addr)
+                if packet[0] == self.INF:
+                    print("Pachet tip informatie inceput")
+                    numberOfPackets, fileName = self.decodeINF(packet)
+                    print(numberOfPackets, " pachete")
+                    print("Nume fisier ", fileName, ", numar de pachete ", numberOfPackets.hex())
+                    file = open(fileName, "wb")
+                if packet[0] == self.Data:
+                    print("Pachet tip date")
+                    packetID, length, dataF = self.decodeData(packet)
+                    print(packetID)
+                    if packetExpected.to_bytes(4, "big") == packetID:
+                        print("Numar de secventa ", packetID.hex(), ", lungime ", length.hex())
+                        toSend = self.encodeAck(packetID)
+                        a = random()
+                        if a>0.03:
+                            s = sock2.sendto(toSend, (addr[0], 5006))
+                            packetExpected += 1
+                            if packetID==numberOfPackets:
+                                print(length)
+                                file.write(packet[9:length+1])
+                                break
+                            else:
+                                file.write(packet[9:])
+                        else:
+                            print("N-am primit")
+
+            sock.close()
+            sock2.close()
+            file.close()
+        except:
+            print("A error");
+
+
+
+
+if __name__ == "__main__":
     UDP_IP = socket.gethostbyname(socket.gethostname())
     UDP_PORT = 5005
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((UDP_IP, UDP_PORT))
-    print(UDP_IP)
-    nr_pachete = 0
-    numeFis = ""
-    numarPachet = 0
-    lungime = 0
-    packetAsteptat = 1
-    file = 0
-    while True:
-        data, addr = sock.recvfrom(1024)
-        packet=bytes(data)
-        print(addr)
-        if packet[0] == INF:
-            print("Pachet tip informatie inceput")
-            nr_pachete = packet[1:5]
-            print(nr_pachete)
-            numeFis = packet[5:]
-            print("Nume fisier ", numeFis, ", numar de pachete ", nr_pachete.hex())
-            file = open(numeFis,"wb")
-        if packet[0] == Data:
-            print("Pachet tip date")
-            numarPachet = packet[1:5]
-            if packetAsteptat.to_bytes(4, "big") == numarPachet:
-                lungime = packet[5:9]
-                print("Numar de secventa ", numarPachet.hex(), ", lungime ", lungime.hex())
-                toSend = b''
-                toSend = toSend + Ack.to_bytes(1, "big") + numarPachet
-                print(toSend.hex())
-                s= sock2.sendto(toSend, (addr[0], 5006))
-
-                #print(s)
-                print("adasd")
-                packetAsteptat += 1
-                if numarPachet==nr_pachete:
-                    print(lungime)
-                    file.write(packet[9:lungime+1])
-                    break
-                else:
-                    file.write(packet[9:])
-
-
-    sock.close()
-    sock2.close()
-    file.close()
-except:
-    print()
+    reciver = Reciver(UDP_IP, UDP_PORT)
+    reciver.start()
